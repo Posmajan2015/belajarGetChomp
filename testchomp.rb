af86c37ebbabe3d330ee589bfe9b3991d7ffcce7
Posmajan2015 <33709274+Posmajan2015@users.noreply.github.com>
2018-04-21 09:20:13 +0700

    Add files via upload

---
+++ testchomp.rb
@@ -0,0 +1,7 @@
+#Blajar gets.chomp
+print "Umur kamu berapa?? "
+umur = gets.chomp.to_i
+print "Kaki kamu berapa?? "
+kaki = gets.chomp.to_i
+print "Jadi umur kamu #{umur} dan kaki kamu #{kaki}  "
+print umur + kaki
